11402d869091c1067bbe32f3a50edacfb84e1587
ali dabbagh <alidabbagh.cp@gmail.com>
2022-09-07 14:52:06 +0430

    navbar toggler was written in js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document
+    .querySelector(".navbar__toggler-image")
+    .addEventListener("click", function () {
+    document.querySelector(".navbar__items").classList.toggle("navbar__items--collapsed");
+});
+
